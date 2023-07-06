b44025c694c2c3f889b956374e3d938517bd785e
lilpocking <lilpocking777@gmail.com>
2023-07-06 14:13:31 +0300

    Little changes

--- TimeWatcher/Model/AppSettings.cs
+++ TimeWatcher/Model/AppSettings.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.Drawing;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-using TimeWatcher.Internal;
 
 namespace TimeWatcher.Model
 {
